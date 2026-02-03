818593f4614bc4829f77f2e1045bc0270fbab70e
yoshiyb1-cmd <yoshiyb1@gmail.com>
2026-02-03 16:41:59 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import { Cormorant_Garamond, Inter } from 'next/font/google';
 
 const serif = Cormorant_Garamond({
   subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-serif'
 });
+
 
 const sans = Inter({
   subsets: ['latin'],
